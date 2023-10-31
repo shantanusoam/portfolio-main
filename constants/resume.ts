692314a0fb30302584698bc7342b07b21ff207fa
shantanu <shantanu@gmail.com>
2023-10-31 16:47:06 +0530

    resume link updated

--- constants/resume.ts
+++ constants/resume.ts
@@ -1,3 +1,4 @@
-export const resume_link= "https://drive.google.com/file/d/1rud8rIMaFeVJ8rSGM1Wv5LofPcpOWvrk/view?usp=sharing";
+export const resume_link =
+  'https://drive.google.com/file/d/1xnDNuEjXyCwPu6Lhyyh6Gbd4vFERVPgf/view?usp=sharing';
 
 export default resume_link;
