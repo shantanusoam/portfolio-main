f6ae7e5fdf1233888f2ecad093c37ab3e18925a8
shantanusoam <51102185+shantanusoam@users.noreply.github.com>
2024-05-18 14:01:24 +0530

    Update resume.ts

--- constants/resume.ts
+++ constants/resume.ts
@@ -1,4 +1,4 @@
 export const resume_link =
-  'https://drive.google.com/file/d/15scBYip-WcPm89SAp-KQYPwuUv8VzXjo/view?usp=sharing';
+  'https://drive.google.com/file/d/1JZgcLXURNGe7f-woBIEMY0b6DGYHV2ch/view?usp=sharing';
 
 export default resume_link;
